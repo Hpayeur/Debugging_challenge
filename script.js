4dfe72ceb67b74b685297898987cb4b8b19dc5c2
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-02 11:48:07 -0700

    commenting

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // make sure these match the HTML element names for the ID then class.
+
+
 //let input = document.querySelector("#NAME");
+
+
 //let display = document.querySelector(".displaymycode");
 
 // This code should be working
+
+
 //input.addEventListener("input", function () {
 //   const name = input.value;
 //   display.textContent = name;
 // });
 
+
 // missing function?
 
 // you should see 5 in the output of your console
+let age =2;
+console.log (age + 3);
+//console.log(7/2); //Float = 3.5
+
 //console.log(simpleMath(2, 3));
